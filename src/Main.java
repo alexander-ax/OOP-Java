b0cd3689b18d4cd375f1a231edb21caf65eca52e
alexander-ax <94437869+alexander-ax@users.noreply.github.com>
2023-06-09 04:29:26 +0800

    Update the first file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world a!");
+        System.out.println("Hello world a nakakainis na!");
     }
 }
