d670fa6d5179156422c7153049a6280cf3d578ae
unknown <you@example.com>
2023-06-26 22:46:33 +0800

    Update the Content

--- src/studentInfo/Student.java
+++ src/studentInfo/Student.java
@@ -3,7 +3,7 @@ package studentInfo;
 
 public class Student {
     public static void main (String[] args){
-        String[] studentsInfo = {"Alexander", "Caberto", "BSCS","A"};
+        String[] studentsInfo = {"Analiza", "Caberto", "BSCS","A"};
         int year = 4;
         double[] grades= {100, 100};
         StudentBlueprint s = new StudentBlueprint(studentsInfo, year, grades);
